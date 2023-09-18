85d45ace1cd1cb48863ce7a54d7cd0fafe05bbdc
miaoyin <miaoyin@woda.com>
2023-09-18 15:44:15 +0800

    fix: html miss

--- projects/carbon/src/lib/pipes/safe-html.pipe.ts
+++ projects/carbon/src/lib/pipes/safe-html.pipe.ts
@@ -7,7 +7,7 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 export class SafeHtmlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(html: string): SafeHtml {
+  transform(html: string | undefined): SafeHtml {
     if (!html) {
       return '';
     }
