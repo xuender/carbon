5d4f45824a646dfa8fb2a69430d11372f6dd97c2
miaoyin <miaoyin@woda.com>
2024-05-27 10:49:29 +0800

    feat: msec Long

--- projects/carbon/src/lib/pipes/msec.pipe.ts
+++ projects/carbon/src/lib/pipes/msec.pipe.ts
@@ -1,16 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { format } from 'date-fns';
+import Long from 'long';
+
+import { toNumber } from './long';
 
 const formatStr = 'yy-MM-dd HH:mm:ss';
 @Pipe({
   name: 'msec',
 })
 export class MsecPipe implements PipeTransform {
-  transform(msec: number | undefined, ...args: string[]): string {
+  transform(msec: number | Long | null | undefined, ...args: string[]): string {
     if (!msec) {
       return '';
     }
 
-    return format(msec, args && args.length ? args[0] : formatStr);
+    return format(toNumber(msec), args && args.length ? args[0] : formatStr);
   }
 }
